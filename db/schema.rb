907e828a7e2eef7c8c72bfe81874195c7a967c0e
Takahiro-Masuda-0306 <taka.baseball.0306@gmail.com>
2022-04-22 12:21:54 +0900

    micropost scaffoldを実行後、migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_22_024713) do
+ActiveRecord::Schema.define(version: 2022_04_22_032103) do
+
+  create_table "microposts", charset: "utf8mb4", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
