544b22c67d9b5b190a70a8b17f253c4349bfc9d9
Takahiro-Masuda-0306 <taka.baseball.0306@gmail.com>
2022-05-06 01:17:13 +0900

    セッション削除（ログアウト）機能を実装

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,10 +1,8 @@
 module SessionsHelper
-  # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
   end
 
-  # ログインユーザーを返す
   def current_user
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
@@ -13,5 +11,10 @@ module SessionsHelper
 
   def logged_in?
     !current_user.nil?
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
